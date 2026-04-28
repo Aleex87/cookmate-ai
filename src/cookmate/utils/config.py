f5590f5896aaf396d1e3aff7632f9dd64d73a07f
Aleex87 <alessandrodanteabbate@gmail.com>
2026-04-28 13:54:29 +0200

    modify the config file

--- src/cookmate/utils/config.py
+++ src/cookmate/utils/config.py
@@ -1,14 +1,6 @@
 from pathlib import Path
 
-def get_project_root() -> Path:
-    current = Path(__file__).resolve()
-    for parent in current.parents:
-        if (parent / "pyproject.toml").exists() and (parent / "src").exists():
-            return parent
-    raise RuntimeError("Project root not found")
-
-
-BASE_DIR = get_project_root()
+BASE_DIR = Path(__file__).parents[1] #get_project_root()
 
 DATA_DIR = BASE_DIR / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
