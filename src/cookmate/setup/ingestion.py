a1f144ef89160400a82430ed827f1568377987a6
Aleex87 <alessandrodanteabbate@gmail.com>
2026-04-28 22:16:37 +0200

    try embedding with cohere but feild for rate_limit changing for SentenceTransformer

--- src/cookmate/setup/ingestion.py
+++ src/cookmate/setup/ingestion.py
@@ -1,11 +1,50 @@
-from src.cookmate.utils.config import PROCESSED_DATA_DIR, DB_DIR
-from sentence_transformers import SentenceTransformer
 import json 
 import lancedb
+from dotenv import load_dotenv
+from lancedb.embeddings import get_registry
+from lancedb.pydantic import LanceModel, Vector
+from src.cookmate.utils.config import PROCESSED_DATA_DIR, DB_DIR
+
+load_dotenv()
+
+embedding_model = get_registry().get("cohere").create(
+    name="embed-multilingual-v3.0"
+)
+
+class RecipeModel(LanceModel):
+    id: str
+    title: str 
+    ingredients: str
+    instructions: str
+    text: str = embedding_model.SourceField()
+    vector: Vector(embedding_model.ndims()) = embedding_model.VectorField()  #type ignore
 
 # Load jason
 with open(PROCESSED_DATA_DIR / "recipes_clean.json") as f:
-    data = json.load(f)
+    recipes = json.load(f)
 
-print(len(data))
-print(data[0])
+records = []
+
+for recipe in recipes:
+    records.append(
+        {
+            "id": recipe["id"],
+            "title": recipe["title"],
+            "ingredients": ", ".join(recipe["ingredients"]),
+            "instructions": " ".join(recipe["instructions"]),
+            "text": recipe["text"],
+        }
+    )
+
+DB_DIR.mkdir(parents=True, exist_ok=True)
+
+vector_db = lancedb.connect(DB_DIR)
+
+table = vector_db.create_table(
+        "recipes",
+        schema=RecipeModel,
+        data=records,
+        mode="overwrite",
+)
+
+print(f"Create table with {table.count_rows()} recipes")
