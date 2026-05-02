902bfc0b969af75caebc406c0811078a1045ceef
Aleex87 <alessandrodanteabbate@gmail.com>
2026-05-02 22:51:07 +0200

    eliminate the placeholder funtion create for the test

--- src/cookmate/backend/agents.py
+++ src/cookmate/backend/agents.py
@@ -9,17 +9,3 @@ def load_recipe_agent_prompt(version: int = 1) -> str:
     prompt = load_prompt(f"prompts:/recipe_agent_system_prompt/{version}")
 
     return prompt.template
-
-def generate_recipes_placeholder(ingredients: list[str]) -> str:
-    """
-    Temporary placeholder function for recipe genertation, this funcion is ment to be removed
-    later end replaced with the full RAG + LLM pipleine.
-    argsuments: ingredients (list[str]): this is the list of the imput ingerdients
-    return: Placeholer responce
-    """
-    prompt = load_recipe_agent_prompt(version=1)
-    return f"""
-        [debug placeholder]
-        loaded prompt: {prompt[:200]}.........
-        ingredient recived: {ingredients}
-    """
