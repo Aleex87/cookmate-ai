4b9950a5aca1563e3c8fb3e43b162c208a3a1cc8
Alicia <alicia.gezelius@gmail.com>
2026-05-03 14:28:18 +0200

    started on basic ui

--- src/cookmate/frontend/app.py
+++ src/cookmate/frontend/app.py
@@ -3,8 +3,15 @@ import httpx
 
 
 def page():
-    pass
+    st.title("Welcome to cookmaite")
+    st.divider()
+    st.header("How does this work?")
+    st.markdown(""" 
+                In the box below you put in the ingrediens you have at home and cookmate will give you a recepic base on what you have at home
+                """)
+    input_ingrediens = st.text_input(label="Put in ingrediens here")
 
+    
 
 def export():
     pass 
